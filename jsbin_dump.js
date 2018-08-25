3623dcb19c88657a85792467bf5bdb72f72a07b2
azarel8586 <azarel.rising@gmail.com>
2018-08-25 16:51:10 -0700

    dumping some play with arrays and ES6

--- jsbin_dump.js
+++ jsbin_dump.js
@@ -50,3 +50,77 @@ let foo = new DataFoo({
   error(err) { console.error(err); },
   complete() { console.log('done')}
 });
+
+// ------------------------------------------------------------------------
+
+// play with MAP and REDUCE
+console.clear();
+
+ // REDUCE
+var pilots = [
+  {
+    id: 10,
+    name: "Poe Dameron",
+    years: 14,
+  },
+  {
+    id: 2,
+    name: "Temmin 'Snap' Wexley",
+    years: 30,
+  },
+  {
+    id: 41,
+    name: "Tallissan Lintra",
+    years: 16,
+  },
+  {
+    id: 99,
+    name: "Ello Asty",
+    years: 22,
+  }
+];
+//to number of years and be slick
+// remember that it returns a value
+const years = pilots.reduce((accumilator, item) => accumilator + item.years, 0);
+console.log(years, "Total years exp");
+
+// the point is that you get a single value back.
+// now find the greenest pilot:
+const newb = pilots.reduce((greenest, item) => {
+  //console.log(greenest, item);
+  if ((greenest.years || 100) > item.years) {
+    return item;
+  } else {
+    // you NEED to pass on the value regardless:
+    return greenest;
+  }
+});
+
+console.log(newb)
+
+// FILTER
+var pilots2 = [
+  {
+    id: 2,
+    name: "Wedge Antilles",
+    faction: "Rebels",
+  },
+  {
+    id: 8,
+    name: "Ciena Ree",
+    faction: "Empire",
+  },
+  {
+    id: 40,
+    name: "Iden Versio",
+    faction: "Empire",
+  },
+  {
+    id: 66,
+    name: "Thane Kyrell",
+    faction: "Rebels",
+  }
+];
+
+console.log(pilots2.filter(item => item.faction === "Rebels"));
+console.log(pilots2.filter(item => item.faction === "Empire"));
